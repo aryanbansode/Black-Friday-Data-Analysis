f6d66a03c6b902a0d1d2324230896bb3c341c0ec
Aryan Bansode <aryanbansode5876@gmail.com>
2026-07-10 10:22:39 +0530

    sample. added

--- Terraform/sample.tf
+++ Terraform/sample.tf
@@ -1,15 +1,14 @@
-provider "aws" {
-    region = "us-east-1"
-}  
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
 
-resource "aws_instance" "myname" {
-    ami           = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
-    key_name = "my-key-pair"
-    vpc_security_group_ids = ["sg-0a1b2c3d4e5f6g7h8"]   
-    tags = {
-        Name = "MyInstance"
-        Environment = "Development"
-    } 
-  
+provider "local" {}
+
+resource "local_file" "example" {
+  filename = "hello.txt"
+  content  = "Terraform is working!"
 }
